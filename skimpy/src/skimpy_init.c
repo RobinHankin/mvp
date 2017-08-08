5b2740499feda9edaf512c4f27407ce3d21730db
RobinHankin <hankin.robin@gmail.com>
2017-08-09 08:49:05 +1200

    add this, created by hand (following the one in spray).  There is an automated method to create this file somewhere but I cannot find it

---
+++ skimpy/src/skimpy_init.c
@@ -0,0 +1,30 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME: 
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .Call calls */
+extern SEXP _skimpy_simplify(SEXP, SEXP, SEXP);
+extern SEXP _skimpy_mvp_prod(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _skimpy_mvp_add(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _skimpy_mvp_power(SEXP, SEXP, SEXP, SEXP);
+extern SEXP _skimpy_mvp_deriv(SEXP, SEXP, SEXP, SEXP);
+
+static const R_CallMethodDef CallEntries[] = {
+    {"_skimpy_simplify",   (DL_FUNC) &_skimpy_simplify, 3},
+    {"_skimpy_mvp_prod",   (DL_FUNC) &_skimpy_simplify, 6},
+    {"_skimpy_mvp_add",   (DL_FUNC) &_skimpy_simplify, 6},
+    {"_skimpy_mvp_power",   (DL_FUNC) &_skimpy_simplify, 4},
+    {"_skimpy_mvp_deriv",   (DL_FUNC) &_skimpy_simplify, 4},
+    {NULL, NULL, 0}
+};
+
+void R_init_spray(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}

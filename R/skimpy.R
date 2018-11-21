7b86264433adf407b365faab5d01df3c237681d9
RobinHankin <hankin.robin@gmail.com>
2018-11-21 21:27:30 +1300

    bugfix

--- R/skimpy.R
+++ R/skimpy.R
@@ -5,15 +5,15 @@
   return(out)
 }
 
+`is.mvp` <- function(x){inherits(x,"mvp")}
+
 `is_ok_mvp` <- function(l){
   stopifnot(is.list(l))
   stopifnot(length(l) == 3)
 
-
   stopifnot(unlist(lapply(l[[1]],is.character)))
   stopifnot(unlist(lapply(l[[2]],is.numeric)))
   stopifnot(is.numeric(l[[3]]))
-
 
   stopifnot(length(l[[1]])==length(l[[2]]))
   stopifnot(length(l[[2]])==length(l[[3]]))
@@ -22,14 +22,11 @@
 
   return(TRUE)
 }
-
   
 `print.mvp` <-  function(x){
-  print("(following is really an mvp object)")
-  print(as.mpoly(x))
-  cat("\n")
-  print("(preceding was really an mvp object)")
-
+    cat("mvp object algebraically equal to\n")
+    print(as.mpoly(x))
+    cat("\n")
 }
 
 `as.mvp` <- function(x){
@@ -38,21 +35,19 @@
   } else if(is.mpoly(x)){
     return(mpoly_to_mvp(x))
   } else if(is.list(x)){
-    return(mvp(x[[1]],x[[2]],x[[3]]))
+    return(mvp(x))
   } else {
     stop("not recognised")
   }
-
 }
 
 `mpoly_to_mvp` <- function(m){
   mvp(list(
-  names  = lapply(m,function(x){names(x[-length(x)])}),
-  powers = lapply(m,function(x){(x[-length(x)])}),
-  coeffs =  unlist(lapply(m,function(x){x[length(x)]}))
+  names  = lapply(m,function(x){names(x[names(x)!="coef"])}),
+  powers = lapply(m, function(x){as.vector(x[names(x)!="coef"])}),
+  coeffs =  unlist(lapply(m,function(x){x["coef"]}))
   ))
 }
-
 
 
 ## as.mpoly() converts out1 into an mpoly object:
@@ -65,24 +60,4 @@
     return(out)
 }
 
-## as.mvp() converts an mpoly object into a mvp object:
-`as.mvp` <- function(X){
-    out <- list()
-    for(i in seq_along(X[[1]])){
-        jj <- X[[i]]
-        jj <- jj[seq_len(length(jj)-1)]
-
-        out$names[[i]] <- names(jj)
-        out$powers[[i]] <- unname(jj)
-        out$coeffs[i] <- unname(jj[length(jj)])
-    }
-
-    class(out) <- "mvp"
-    return(out)
-}
     
-
-                 
-    
-
-   

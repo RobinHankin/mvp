66a3feda4f095e8fa0649ffb7c86da6b1a8274de
RobinHankin <hankin.robin@gmail.com>
2018-11-21 14:22:34 +1300

    middle complexity example

---
+++ tests/aab.R
@@ -0,0 +1,26 @@
+library(skimpy)
+library(magic)
+library(mpoly)
+
+
+# 1+3a+7a^2*b + 9abc:
+allnames <- list(
+    c("a"),
+    c("a"),
+    c("a","b"),
+    letters[1:3]
+)
+
+
+allpowers <- list(
+    0,
+    1,
+    c(2,1),
+    c(1,1,1)
+    )
+
+coeffs = c(1,3,7,9)
+
+
+out1 <- simplify(allnames=allnames,allpowers=allpowers,coefficients=coeffs)
+ 

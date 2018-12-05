f0cb36e88df622014697acc189fc7c61396363a8
RobinHankin <hankin.robin@gmail.com>
2018-12-05 14:16:47 +1300

    incorporate kahle() and document it

--- R/skimpy.R
+++ R/skimpy.R
@@ -205,3 +205,19 @@ setGeneric("aderiv",function(x){standardGeneric("aderiv")})
   mvp(vp,pp,coeffs(p))
 }
 
+
+`kahle`  <- function(n=26,r=1,p=1,coeffs=1,symbols=letters){
+  if(n > length(symbols)){stop("not enough symbols")}
+  symbols <- symbols[seq_len(n)]
+  p <- rep(p,length=r)
+  jj <- circulant(symbols)[seq_len(r),]
+  jj <- split(jj,rep(seq_len(n),each=r))
+
+  coeffs=rep(coeffs,length=n)
+
+  mvp(
+      vars   = jj,
+      powers = rep(list(p),n),
+      coeffs = coeffs
+  )
+}

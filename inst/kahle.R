264bb05419d971fa814c08d8c88b3035413a0656
RobinHankin <hankin.robin@gmail.com>
2018-12-05 13:37:33 +1300

    bugfix

--- inst/kahle.R
+++ inst/kahle.R
@@ -1,19 +1,27 @@
 library("mvp")
+library("magic")
 
-shift <- function(x,i=1){# from magic
-  n <- length(x)
-  return(x[c((n - i + 1):n, 1:(n - i))])
-} 
- 
-kahle  <- mvp(
+kahle  <- function(n,r=2,p=1,coeffs=1,symbols=letters){
+  if(n > length(symbols)){stop("not enough symbols")}
+  symbols <- symbols[seq_len(n)]
+  p <- rep(p,length=r)
+  jj <- circulant(symbols)[seq_len(r),]
+  jj <- split(jj,rep(seq_len(n),each=r))
+
+  coeffs=rep(coeffs,length=n)
+
+  mvp(
+      vars   = jj,
+      powers = rep(list(p),n),
+      coeffs = coeffs
+  )
+}
+
+kahleold  <- mvp(
     vars     = split(cbind(letters,shift(letters)),rep(seq_len(26),each=2)),
     powers    = rep(list(1:2),26),
     coeffs = 1:26
 )
-
-
-
-
 
 
 

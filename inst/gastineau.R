6a89b7cf991b22aba443bdea473cd317a04f91c3
RobinHankin <hankin.robin@gmail.com>
2019-01-05 18:06:34 +1300

    give proper reference

--- inst/gastineau.R
+++ inst/gastineau.R
@@ -1,4 +1,13 @@
-## Following example taken from M. Gastineau, who considers p*(p+1) /. {p -> (1+x+y+z+t)^16
+## Following example taken from:
+
+
+## M. Gastineau and J. Laskar, 2006.  "Development of TRIP: Fast
+## Sparse Multivariate Polynomial Multiplication Usig Burst Tries" In
+## V. N. Alexandrov et al. (Eds.): ICCS 2006, Part II, LNCS 3992,
+## pp446-453, 2006.
+
+## These authors consider p*(p+1) /. {p -> (1+x+y+z+t)^16 using
+## different systems such as pari/gp.
 
 
 library(skimpy)

b3071d363a613e1b1a6150689325a6a8df6a166c
RobinHankin <hankin.robin@gmail.com>
2019-11-10 11:33:28 +1300

    coverage of skimpy.R now 100%

--- tests/testthat/test_aaf.R
+++ tests/testthat/test_aaf.R
@@ -103,4 +103,29 @@ test_that("Test suite test_aaf.R",{
         foo(x,y)
     }
 
+    expect_error(kahle(symbols=letters[1:2]))
+    expect_true(horner("x",1:4) == as.mvp("1 + 2 x + 3 x^2 + 4 x^3"))
+    expect_true(trunc(horner("x+y",1:4),1) == as.mvp("1 + 2 x + 2 y"))
+    expect_true(trunc1(as.mvp("1+x+y^2")^4,x=1,y=2) == as.mvp("1+4 x + 12 x y^2 + 4 y^2"))
+
+
+    P <- horner("x+y^3 + x*y",1:7)-1
+    expect_true(is.constant(ooom(P,0)))
+    expect_true(is.zero(ooom(P,0)-1))
+    expect_true(is.zero(trunc(ooom(P,7)*(1-P)-1,7)))
+
+    expect_true(is.constant(onevarpow(P,y=18)))
+
+    expect_output(print(series(P,"x")))
+    
+    expect_true(varchange(varchange(P,x="xux"),xux="x")==P)
+    expect_true(varchange_formal(varchange_formal(P,"x","_"),"_","x") == P)
+
+    expect_silent(taylor(P,"x","a"))
+    expect_silent(taylor(P,"x","a",debug=TRUE))
+    expect_true(nterms(P)==73)
+    expect_output(print(summary(P)))
+    expect_silent(rtypical(P))
+
 })
+

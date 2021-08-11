e3e6c3f120645fdd6ec6bfd7b65620787974bf8d
Robin Hankin <hankin.robin@gmail.com>
2021-08-12 11:28:02 +1200

    make font larger on sticker

--- inst/mvp_stickermaker.R
+++ inst/mvp_stickermaker.R
@@ -1,5 +1,5 @@
 library("hexSticker")
 
-sticker("mvp_blue.png", package="mvp", p_size=8, s_x=1, s_y=.95,
+sticker("mvp_blue.png", package="mvp", p_size=13, s_x=1, s_y=.95,
 s_width=1, asp=0.85, white_around_sticker=TRUE, h_fill="#7733FF",
 h_color="#000000", filename="mvp.png")
